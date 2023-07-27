8f0b88ccdef8c3a729951c40ae96b892f12b1783
KseniyaVKS <133357376+KseniyaVKS@users.noreply.github.com>
2023-07-27 10:19:40 +0600

    Update Staff.sql

--- Staff.sql
+++ Staff.sql
@@ -1,10 +1,6 @@
-CREATE TABLE IF NOT EXISTS Chiefs (
-	chief_id SERIAL PRIMARY KEY
-);
-
 CREATE TABLE IF NOT EXISTS Employeis (
 	employee_id SERIAL PRIMARY KEY,	
 	name VARCHAR(40) NOT null,
 	department VARCHAR(255) NOT null,
-	chief_id INTEGER NOT NULL REFERENCES Chiefs(chief_id)
+	chief_id INTEGER
 );
