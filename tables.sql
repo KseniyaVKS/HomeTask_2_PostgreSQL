fbdbec83de50ca27acd27a6883bddb74aed8cbd9
KseniyaVKS <133357376+KseniyaVKS@users.noreply.github.com>
2023-07-27 10:20:44 +0600

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS Musicians (
 
 CREATE TABLE IF NOT EXISTS Genres (
 	genre_id SERIAL PRIMARY KEY,	
-	name VARCHAR(40) NOT NULL	
+	name VARCHAR(40) UNIQUE	
 );
 
 CREATE TABLE IF NOT EXISTS genre_musician (
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS genre_musician (
 CREATE TABLE IF NOT EXISTS Albums (
 	album_id SERIAL PRIMARY KEY,	
 	name VARCHAR(40) NOT null,
-	release INTEGER NOT NULL
+	release INTEGER CHECK(release < 5) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS album_musician (
@@ -29,13 +29,19 @@ CREATE TABLE IF NOT EXISTS album_musician (
 CREATE TABLE IF NOT EXISTS Tracks (
 	track_id SERIAL PRIMARY KEY,	
 	name VARCHAR(40) NOT null,
-	duration time not null,
+	duration INTEGER CHECK(duration < 11) not null,
 	album_id INTEGER NOT NULL REFERENCES Albums(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS Collections (
 	collection_id SERIAL PRIMARY KEY,	
 	name VARCHAR(40) NOT null,
-	release INTEGER NOT NULL,
-	track_id INTEGER NOT NULL REFERENCES Tracks(track_id)
+	release INTEGER CHECK(release < 5) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS track_collection (
+	t_c_id SERIAL PRIMARY KEY,
+	track_id INTEGER NOT NULL REFERENCES Tracks(track_id),
+	collection_id INTEGER NOT NULL REFERENCES Collections(collection_id)
 );
